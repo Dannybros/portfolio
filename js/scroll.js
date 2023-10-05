46130d6ef1c0a6ef41ab52d7e54b7eb3e170b805
Dannybros <akbaromega89@gmail.com>
2023-10-05 08:15:32 +0700

    changing scroll animation

--- js/scroll.js
+++ js/scroll.js
@@ -2,9 +2,10 @@ function scrollAnimationDirection(direction){
     
     let observer = new IntersectionObserver(entries=>{
         entries.forEach((entry) => {
+            // entry.target.classList.add(`swipe-${direction}-show`);
             entry.target.classList.toggle(`swipe-${direction}-show`, entry.isIntersecting);
         });
-    },{ rootMargin: "-50px" });
+    },{ threshold: [0.25]});
     
     const hiddenElements = document.querySelectorAll(`.swipe-${direction}-hidden`);
     hiddenElements.forEach((el)=>observer.observe(el));
